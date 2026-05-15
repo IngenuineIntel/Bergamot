8393624145198017d72a16d46a0a4f331500064c
IngenuineIntel <roan.rothrock@proton.me>
2026-05-14 20:08:30 -0500

    Found a better way

--- overseer/sql/retreivetimestamps.sql
+++
@@ -1,7 +0,0 @@
--- retreiveextremetimestamps.sql
--- retreiving max and min timestamps for view
-
-SELECT
-    MAX(ts_s) as max,
-    MIN(ts_s) as min
-FROM events; -- or procs, or system_perf, doesn't matter
